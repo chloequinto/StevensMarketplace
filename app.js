dbbd450746deed3f152e4af44319f1d9cba3da45
chloequinto <cquinto@stevens.edu>
2020-05-12 00:56:59 -0400

    middlewares for edit listing and profile

--- app.js
+++ app.js
@@ -148,6 +148,44 @@ app.use("/yourListings", (req, res, next)=> {
     }
 });
 
+
+app.use("/removeCart", (req, res, next)=> { 
+	if (!req.session.user){
+        let authenticated = "Non-Authenticated User (Middleware)"
+		console.log('[' + new Date().toUTCString()  + ']: ' + req.method + " " + req.originalUrl+ " " + authenticated)
+        return res.redirect('/login') 
+    }else { 
+        let authenticated = "Authenticated User (Middleware)"
+		console.log('[' + new Date().toUTCString()  + ']: ' + req.method + " " + req.originalUrl+ " " + authenticated)
+        next(); 
+    }
+});
+
+
+app.use("/editListing", (req, res, next)=> { 
+	if (!req.session.user){
+        let authenticated = "Non-Authenticated User (Middleware)"
+		console.log('[' + new Date().toUTCString()  + ']: ' + req.method + " " + req.originalUrl+ " " + authenticated)
+        return res.redirect('/login') 
+    }else { 
+        let authenticated = "Authenticated User (Middleware)"
+		console.log('[' + new Date().toUTCString()  + ']: ' + req.method + " " + req.originalUrl+ " " + authenticated)
+        next(); 
+    }
+});
+
+app.use("/profile", (req, res, next)=> { 
+	if (!req.session.user){
+        let authenticated = "Non-Authenticated User (Middleware)"
+		console.log('[' + new Date().toUTCString()  + ']: ' + req.method + " " + req.originalUrl+ " " + authenticated)
+        return res.redirect('/login') 
+    }else { 
+        let authenticated = "Authenticated User (Middleware)"
+		console.log('[' + new Date().toUTCString()  + ']: ' + req.method + " " + req.originalUrl+ " " + authenticated)
+        next(); 
+    }
+});
+
 configRoutes(app);
 
 app.listen(3000, () => {
